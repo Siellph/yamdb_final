0b02b7a55b44e971bb3c0cf40d73067621d54eef
Vladislav <gordin@it-impuls.ru>
2023-03-24 14:51:55 +0500

    fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,10 +8,10 @@ from django.conf.global_settings import DATETIME_INPUT_FORMATS
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', default='None')
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'False'
+DEBUG = os.getenv('DEBUG', default='False')
 
 ALLOWED_HOSTS = ['*']
 
